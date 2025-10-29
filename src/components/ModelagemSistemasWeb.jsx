9bf1988fecb9dd46d3f8e2bb51707de7ea193b5d
AndrewHGomes <andrewvianigomes@gmail.com>
2025-10-29 06:22:48 -0300

    Análise Estruturada em Modelagem de Sistemas Web

--- src/components/ModelagemSistemasWeb.jsx
+++ src/components/ModelagemSistemasWeb.jsx
@@ -4,6 +4,11 @@ import aula2 from "../assets/audiosM8/aula02.mp3";
 import aula3 from "../assets/audiosM8/aula03.mp3";
 import aula4 from "../assets/audiosM8/aula04.mp3";
 
+import aula5 from "../assets/audiosM8/aula05.mp3";
+import aula6 from "../assets/audiosM8/aula06.mp3";
+import aula7 from "../assets/audiosM8/aula07.mp3";
+import aula8 from "../assets/audiosM8/aula08.mp3";
+
 const ModelagemSistemasWeb = () => {
   const [abrir, setAbrir] = useState(false);
   const [girarImg, setGirarImg] = useState(false);
@@ -51,16 +56,15 @@ const ModelagemSistemasWeb = () => {
             </p>
             <p>O modelo teórico de um processo envolve:</p>{" "}
             <ul>
-              {" "}
               <li>
                 Recebimento de <strong>Insumos de Entrada</strong>;
-              </li>{" "}
+              </li>
               <li>
                 <strong>Processamento</strong> desses insumos;
-              </li>{" "}
+              </li>
               <li>
                 Geração de um <strong>Resultado (Saída)</strong>.
-              </li>{" "}
+              </li>
             </ul>
             <h4>A Importância da Documentação e Mapeamento</h4>
             <p>
@@ -84,20 +88,19 @@ const ModelagemSistemasWeb = () => {
               A gestão de processos envolve duas etapas principais de modelagem:
             </p>
             <ul>
-              {" "}
               <li>
                 <strong>AS IS ("Como É"):</strong> É a "fotografia" do processo
                 atual. Consiste em <strong>identificar e mapear</strong> o passo
                 a passo de como o processo é realmente executado no dia a dia,
                 entrevistando e observando os profissionais envolvidos.
-              </li>{" "}
+              </li>
               <li>
                 <strong>TO BE ("Como Será"):</strong> É a proposta de um{" "}
                 <strong>novo processo ou a melhoria</strong> do processo AS IS,
                 visando torná-lo mais <strong>eficiente e ágil</strong>. O novo
                 fluxo deve ter um <strong>propósito claro</strong> e trazer
                 ganhos de produtividade e qualidade.
-              </li>{" "}
+              </li>
             </ul>
             <p>
               Para construir o TO BE, é necessário um pensamento inovador,
@@ -122,30 +125,181 @@ const ModelagemSistemasWeb = () => {
               a finalização:
             </p>
             <ul>
-              {" "}
               <li>
                 <strong>Eventos (Círculos):</strong> Indicam o início (verde) ou
                 o fim (vermelho) do processo.
-              </li>{" "}
+              </li>
               <li>
                 <strong>Atividades/Tarefas (Retângulos):</strong> Representam o{" "}
                 <strong>trabalho realizado</strong> em uma etapa do processo.
-              </li>{" "}
+              </li>
               <li>
                 <strong>Fluxo de Sequência (Seta):</strong> Conector que mostra
                 a ordem de execução das atividades.
-              </li>{" "}
+              </li>
               <li>
                 <strong>Gateway (Losango):</strong> Elemento de{" "}
                 <strong>decisão</strong> que separa ou junta os diferentes
                 caminhos lógicos do processo.
-              </li>{" "}
+              </li>
             </ul>
             <p>
               A modelagem do processo de negócio é, portanto, o primeiro passo
               da modelagem de sistemas e da construção de um software, pois ela
               identifica as partes do fluxo que podem e devem ser{" "}
               <strong>automatizadas</strong>.
+            </p>
+          </div>
+          <div>
+            <h3>Análise Estruturada e Modelagem de Processos de Negócio</h3>
+            <div className="audios">
+              <audio src={aula5} controls></audio>
+              <audio src={aula6} controls></audio>
+              <audio src={aula7} controls></audio>
+              <audio src={aula8} controls></audio>
+            </div>
+            <p>
+              O conhecimento em análise de sistemas é{" "}
+              <strong>fundamental</strong> para o desenvolvimento de software.
+              Ele permite traduzir as necessidades de negócio do cliente em um{" "}
+              <strong>projeto técnico planejado e documentado</strong>,
+              garantindo que os objetivos de negócio sejam atendidos com um
+              processamento <strong>rápido e eficiente</strong>. Construir
+              software é uma atividade complexa que exige planejamento
+              estratégico antes da codificação.
+            </p>
+            <p>
+              Devido à crescente necessidade por programas e à evolução
+              tecnológica, foi preciso estruturar a construção de software. A
+              análise de sistemas surgiu, definindo formas mais efetivas de
+              desenvolver sistemas <strong>confiáveis</strong> e com{" "}
+              <strong>custo adequado</strong>.{" "}
+            </p>
+            <p>
+              Análise de sistemas é uma técnica que visa encontrar o{" "}
+              <strong>melhor caminho lógico</strong> para o processamento de
+              informações. A modelagem de sistemas, por sua vez, é uma prática
+              baseada em modelos, buscando um melhor entendimento da entidade
+              real a ser construída. O modelo deve representar:
+            </p>
+            <ul>
+              <li>
+                <strong>Entidades físicas</strong> (ex: aluno, cliente) e suas
+                informações.
+              </li>
+              <li>
+                As <strong>funcionalidades</strong> (solução técnica) que o
+                software precisa entregar, tanto do ponto de vista do cliente
+                quanto do técnico.
+              </li>
+            </ul>
+            <p>
+              Ao longo do tempo, a evolução da análise de sistemas propôs três
+              grandes métodos de modelagem:
+            </p>
+            <ul>
+              <li>Modelagem Estruturada</li> <li>Modelagem Essencial</li>{" "}
+              <li>Modelagem Orientada a Objetos</li>{" "}
+            </ul>
+            <h4>Modelagem Estruturada</h4>
+            <p>
+              A análise estruturada é um método de modelagem que se fundamenta
+              em <strong>quebrar um problema em partes menores</strong> para
+              facilitar a busca pela melhor solução. Seu foco principal é nas
+              <strong>funcionalidades</strong> que o software deve entregar.
+              Utiliza um conjunto de ferramentas para organizar a análise, sendo
+              o <strong>Diagrama de Fluxo de Dados (DFD)</strong> e o{" "}
+              <strong>Dicionário de Dados (DD)</strong> as principais.
+            </p>
+            <p>
+              O objetivo principal é a construção de um{" "}
+              <strong>modelo lógico</strong>, buscando compreender a lógica por
+              trás de cada funcionalidade. Os modelos visuais produzidos devem
+              fornecer um quadro claro e geral do sistema. A especificação do
+              sistema, baseada na análise estruturada, é o elo entre a análise e
+              o projeto e é composta por DFD, DD e especificações de processos.
+            </p>
+            <h4>Diagrama de Fluxo de Dados (DFD)</h4>
+            <p>
+              O DFD é uma ferramenta essencial para compreender e analisar o
+              <strong>fluxo de dados</strong> (entrada, processamento e saída)
+              no sistema e entre ele e o ambiente externo. Ele mostra, por meio
+              de uma representação em rede, as funcionalidades que o sistema
+              deve entregar e os dados que as interligam. O DFD foca no{" "}
+              <strong>O QUE</strong> o sistema faz (análise), e não no COMO
+              (projeto).
+            </p>
+            <p>
+              A criação do DFD deve seguir boas práticas, como escolher{" "}
+              <strong>nomes significativos, numerar os processos</strong> e
+              utilizar o <strong>particionamento em níveis</strong> (quebrar o
+              detalhamento) para facilitar a compreensão de um problema
+              complexo.
+            </p>
+            <p>O diagrama é composto pelos seguintes elementos:</p>
+            <ul>
+              {" "}
+              <li>
+                <strong>Processo (ou Bolha):</strong> É o coração do DFD. Mostra
+                as funcionalidades que transformam dados de entrada em dados de
+                saída. Devem ter nomes claros e diretos, utilizando{" "}
+                <strong>verbos</strong> (ex: Armazenar dados do cliente ). Podem
+                representar ações manuais ou automatizadas.
+              </li>{" "}
+              <li>
+                <strong>Fluxo de Dados:</strong> Interligam os processos.
+                Representam os caminhos por onde os dados circulam, indicados
+                por <strong>setas</strong> (origem para destino). Cada fluxo
+                deve ter um único nome, que identifica os dados transportados
+                (ex: Pedido).
+              </li>
+              <li>
+                <strong>Depósito de Dados:</strong> Representa o{" "}
+                <strong>armazenamento</strong>
+                de dados para uso futuro. O nome deve estar sempre no{" "}
+                <strong>plural</strong>
+                (ex: Clientes).
+              </li>
+              <li>
+                <strong>Entidade (ou Terminador):</strong> Fontes ou
+                destinatários das informações que entram ou saem do sistema
+                (pessoas, coisas ou outro sistema externo). O nome deve estar no
+                <strong>plural</strong> e em <strong>maiúsculo</strong> (ex:
+                CLIENTES, SISTEMA DE CONTABILIDADE).
+              </li>
+            </ul>
+            <h4>Organização e Níveis do DFD</h4>
+            <p>
+              Para facilitar a visualização e manutenção, o DFD é organizado em
+              uma série de níveis, que oferecem sucessivamente mais detalhes
+              sobre o sistema. O objetivo é ir do nível mais macro ao mais
+              detalhado. O limite de criação de níveis é o{" "}
+              <strong>completo entendimento do problema</strong>.
+            </p>
+            <ul>
+              <li>
+                <strong>DFD de Contexto (Nível Mais Alto):</strong> Oferece a
+                visão macro. Contém apenas <strong>um processo</strong> que
+                representa o sistema todo, os principais fluxos de dados e os
+                <strong>agentes externos</strong> (entidades).
+              </li>
+              <li>
+                <strong>DFD de Nível Zero:</strong> Analisa os{" "}
+                <strong>principais processos</strong> do software, detalhando os
+                fluxos e entidades envolvidas nessas funções.
+              </li>
+              <li>
+                <strong>DFDs de Níveis Intermediários:</strong> Mostram a
+                <strong>decomposição</strong> e o detalhamento de cada processo
+                modelado nos níveis superiores, até que todos os relacionamentos
+                de dados estejam claros.
+              </li>
+            </ul>
+            <p>
+              O principal benefício de usar o DFD na modelagem é a sua
+              <strong>simplicidade</strong>, que permite a avaliação do modelo
+              junto aos usuários e a{" "}
+              <strong>identificação precoce de falhas</strong> nos requisitos.
             </p>
           </div>
         </>
